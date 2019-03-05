3e9288a1b0b7ddfb38bcbecfb8fc26fece095996
InkInn <lrc19931109@outlook.com>
2019-03-05 10:41:02 +0800

    update

--- src/main/java/interceptor/controller/TestController.java
+++ src/main/java/interceptor/controller/TestController.java
@@ -2,13 +2,15 @@ package interceptor.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
 
     @RequestMapping("/test")
+    @ResponseBody
     public String test(){
-        return "哈哈哈哈";
+        return "lasldlasldas";
     }
 }
