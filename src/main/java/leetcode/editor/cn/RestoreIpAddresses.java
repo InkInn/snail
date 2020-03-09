eb2d7c11ca2c6deadc2d2d19723364c9fd049ec6
InkInn <lrc19931109@outlook.com>
2020-03-09 16:52:06 +0800

    Update RestoreIpAddresses.java

--- src/main/java/leetcode/editor/cn/RestoreIpAddresses.java
+++ src/main/java/leetcode/editor/cn/RestoreIpAddresses.java
@@ -23,7 +23,7 @@ public class RestoreIpAddresses {
     class Solution {
         public List<String> restoreIpAddresses(String s) {
             List<String> result = new ArrayList<>();
-            StringBuilder ip = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
             for (int a = 1; a < 4; a++)
                 for (int b = 1; b < 4; b++)
                     for (int c = 1; c < 4; c++)
@@ -32,11 +32,14 @@ public class RestoreIpAddresses {
                                 int n1 = Integer.parseInt(s.substring(0, a));
                                 int n2 = Integer.parseInt(s.substring(a, a + b));
                                 int n3 = Integer.parseInt(s.substring(a + b, a + b + c));
-                                int n4 = Integer.parseInt(s.substring((a + b + c)));
+                                int n4 = Integer.parseInt(s.substring(a + b + c));
                                 if (n1 <= 255 && n2 <= 255 && n3 <= 255 && n4 <= 255) {
-                                    ip.append(n1).append(".").append(n2).append(".").append(n3).append(".").append(n4);
-                                    if (ip.length() == s.length() + 3) result.add(ip.toString());
-                                    ip.delete(0, ip.length());
+                                    sb.append(n1).append(".").append(n2).append(".").append(n3).append(".").append(n4);
+                                    if (sb.length() == s.length() + 3) {
+                                        result.add(sb.toString());
+
+                                    }
+                                    sb.delete(0, sb.length());
                                 }
                             }
                         }
